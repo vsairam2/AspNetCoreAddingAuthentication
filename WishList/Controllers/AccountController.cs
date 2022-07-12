fe05fad7403d81ad816a3cb9c922b47320a2ff13
sairam <sairamvarma00@gmail.com>
2022-07-12 07:23:12 +0000

    module5

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -3,24 +3,29 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
 using WishList.Models.AccountViewModels;
+
 namespace WishList.Controllers
 {
+
     [Authorize]
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+
         public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
         [HttpGet]
         [AllowAnonymous]
         public IActionResult Register()
         {
             return View();
         }
+
         [HttpPost]
         [AllowAnonymous]
         public IActionResult Register(RegisterViewModel model)
