e0d19417d0b42cbf90dec472b604c9e7dcc90427
sairam <sairamvarma00@gmail.com>
2022-07-13 07:39:41 +0000

    model9

---
+++ WishList/Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class LoginViewModel
+    {
+        [Required, EmailAddressAttribute]
+        public string Email { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
