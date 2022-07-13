0ab1a3ce0515a787d41d7e52e214f18fcf196be4
sairam <sairamvarma00@gmail.com>
2022-07-13 07:21:03 +0000

    model8

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -2,38 +2,38 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
+using WishList.Models.AccountViewModels;
 
 namespace WishList.Controllers
 {
-
     [Authorize]
     public class AccountController : Controller
     {
-        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly UserManager<ApplicationUser> userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-
         public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
         }
-
-        [HttpGet]
-        [AllowAnonymous]
+    
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet,AllowAnonymous]
         public IActionResult Register()
         {
             return View();
         }
-
         [HttpPost]
         [AllowAnonymous]
         public IActionResult Register(RegisterViewModel model)
         {
-            if (!ModelState.IsValid)
-                return View(model);
-
+            if(!ModelState.IsValid)
+            return View(model);
+            
             var result = _userManager.CreateAsync(new ApplicationUser() { Email = model.Email, UserName = model.Email }, model.Password).Result;
-
             if (!result.Succeeded)
             {
                 foreach (var error in result.Errors)
@@ -42,7 +42,32 @@ namespace WishList.Controllers
                 }
                 return View(model);
             }
-
+            return RedirectToAction("Index","Home");
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost,AllowAnonymous,ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var result = _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false).Result;
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(model);
+            }
+            return RedirectToAction("Index", "Item");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
     }
--- WishList/Models/AccountViewModels/RegisterViewModel.cs
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -13,9 +13,9 @@ namespace WishList.Model.AccountViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [Compare("Password")]
+        [Required, DataType(DataType.Password), CompareAttribute("Password",ErrorMessage = "The New Password and Confirm New Password fields did not match.")]
+       
+       
         public string ConfirmPassword { get; set; }
     }
 }
